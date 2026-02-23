6530c1d4ac187260e45e6d1ab25cfc924065d598
Sara-Fioretti <fiorettisara22@gmail.com>
2026-02-23 10:41:23 +0100

    Snack2

--- script/myscript.js
+++ script/myscript.js
@@ -15,7 +15,7 @@ let oggetti = [
 ]
 
 function elementoMinore(array) {
-    let pesoMinore=array[0].peso
+    let pesoMinore = array[0].peso
     for (let i = 0; i < oggetti.length; i++) {
         if (oggetti[i].peso < oggetti[0].peso) {
             pesoMinore = oggetti[i].peso
@@ -32,3 +32,29 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
+
+let squadre = [
+    { nome: "Roma", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Milan", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Napoli", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Fiorentina", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Inter", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Juventus", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Cremonese", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Cagliari", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Lazio", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Atalanta", puntiFatti: 0, falliSubiti: 0 },
+]
+
+for (let i = 0; i < squadre.length; i++) {
+    squadre[i].puntiFatti = Math.floor(Math.random()*70);
+    squadre[i].falliSubiti = Math.floor(Math.random()*500);
+}
+console.log(squadre)
+
+let falliSquadre = [];
+for (let i = 0; i < squadre.length; i++) {
+    falliSquadre.push(squadre[i].nome)
+    falliSquadre.push(squadre[i].falliSubiti)
+}
+console.log(falliSquadre)
